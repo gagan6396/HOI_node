aba61d4f84d4398eadeb32a208abc1a1e4b29860
Arun Negi <Arun-Negi989>
2025-12-09 12:29:56 +0530

    env issue

--- server.js
+++ server.js
@@ -33,4 +33,6 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// 👇 Change this line
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
